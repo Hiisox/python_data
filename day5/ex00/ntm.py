f546bb6792b37cf1c38b0256f8b506ec2edba6ed
hiisox <mehdmhaya@gmail.com>
2024-07-26 04:12:36 +0200

    k

--- day5/ex00/ntm.py
+++ day5/ex00/ntm.py
@@ -1,3 +1,13 @@
+def ft_statistics(*args: any, **kwargs: any) -> None:
+    if checker(args, kwargs) is False:
+        print("ERROR")
+    myList = [x for x in args]
+    myList.sort()
+    # mean(mylist)
+    # median(mylist)
+    # quartile(mylist)
+    # var(myList)
+
 
 def checker(*args:any, **kwargs:any) -> bool:
     for x in args[0]:
@@ -11,7 +21,7 @@ def mean(myList: list):
     for x in myList:
         mean += x
     mean /= len(myList)
-    print(mean)
+    return mean
 
 
 def median(myList: list):
@@ -22,45 +32,36 @@ def median(myList: list):
         print(myList[index])
     else :
         index = int(index / 2 - 1)
-        print((myList[index] + myList[index + 1]) / 2)
+        return (myList[index] + myList[index + 1]) / 2
 
 
 def quartile(myList: list):
     value = len(myList)
     value2 = value
-    value2 = (value2 + 1)/ 4
+    value2 = value2/ 4
     quartile = []
-    if value2 == int(value2):
-        quartile.append(myList[value2 - 1])
-    else:
-        print(value2)
-        x = value2
-        while (x > 1):
-            x -= 1
-        value2 = int(value2)
-        quartile.append((1 - x) * myList[value2 - 1] + x * myList[value2])
-    value2 = 3 * ((value + 1 )/ 4)
-    if value2 == int(value2):
-        quartile.append(myList[value2 - 1])
-    else:
-        x = value2
-        while (x > 1):
-            x -= 1
-        value2 = int(value2)
-        quartile.append((1 - x) * myList[value2 - 1] + x * myList[value2])
-    print(quartile)
+    value2 = int(value2)
+    quartile.append(myList[value2])
+    value2 = (3 * value)/ 4
+    value2 = int(value2)
+    quartile.append(myList[value2])
+    return quartile
 
 
-def ft_statistics(*args: any, **kwargs: any) -> None:
-    if checker(args, kwargs) is False:
-        print("ERROR")
-    mylist = [x for x in args]
-    mylist.sort()
-    # mean(mylist)
-    # median(mylist)
-    quartile(mylist)
 
-    return
+def var(myList: list):
+    mean = mean(myList)
+    var = 0
+    for value in myList:
+        x = value - mean
+        var = var + x * x
+    var = var / (len(myList))
+    return var
 
 
-ft_statistics(1, 11, 42, 64, 360)
+def racine_carre(number: float):
+    print(str(number), type(str(number)))
+    pass
+
+# ft_statistics(5, 75, 450, 18, 597, 27474, 48575)
+racine_carre(10.42)
